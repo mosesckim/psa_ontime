22edfb95236c75b0d50a5b48e01e1352e67b5c14
Moses Kim <moses@thinkingmachin.es>
2022-12-02 20:00:54 +0800

    Add macroecnomic page

--- pages/macroeconomic.py
+++ pages/macroeconomic.py
@@ -3,9 +3,13 @@ import datetime
 import streamlit as st
 import altair as alt
 import pandas as pd
+import numpy as np
 
 from google.oauth2 import service_account
 from google.cloud import storage
+from collections import defaultdict
+
+from utils import process_schedule_data, restrict_by_coverage, get_stats
 
 
 st.set_page_config(page_title="Macroeconomic", page_icon="🚢")
@@ -42,6 +46,155 @@ def read_file(bucket_name, file_path, is_csv=True, sheet=None):
     return pd.read_excel(data_bytes, sheet)
 
 bucket_name = "psa_ontime_streamlit"
+
+# schedule analysis
+
+
+file_path = "2022-11-29TableMapping_Reliability-SCHEDULE_RELIABILITY_PP.csv"
+# read in reliability schedule data
+schedule_data = read_file(bucket_name, file_path)
+
+rel_df_nona = process_schedule_data(schedule_data)
+rel_df_nona = restrict_by_coverage(rel_df_nona)
+
+
+
+rel_df_nona_trade = rel_df_nona.copy()
+
+trade_options = rel_df_nona_trade["Trade"].unique()
+
+with st.sidebar:
+    trade_option = st.selectbox(
+        'Trade: ',
+        trade_options)
+
+
+months = list(rel_df_nona_trade["Month(int)"].unique())
+
+rel_df_nona_trade = rel_df_nona_trade[
+    rel_df_nona_trade["Trade"]==trade_option
+]
+
+agg_cols = [
+    "POL",
+    "POD",
+    "Carrier",
+    "Service"
+]
+
+reL_df_nona_delay_outlier = rel_df_nona_trade.groupby(
+    agg_cols
+).apply(get_stats).reset_index()
+
+
+
+# prepare data frame for grouped bar chart
+res_dict = defaultdict(list)
+
+months = [col for col in reL_df_nona_delay_outlier.columns if str(col).isnumeric()]
+
+for ind, row in reL_df_nona_delay_outlier.iterrows():
+
+    for month in months:
+        delay, outlier = row[month]
+
+        res_dict["Month"].append(month)
+        res_dict["Type"].append("delay")
+        res_dict["Count"].append(int(delay))
+
+        res_dict["Month"].append(month)
+        res_dict["Type"].append("outlier")
+        res_dict["Count"].append(int(outlier))
+
+res_df = pd.DataFrame(res_dict)
+
+
+
+# DELAY ANALYSIS BY SPECIFIC ROUTE
+
+# helper method
+def convert_to_frame(delay_dict):
+
+    route_delay_df = pd.DataFrame(
+        delay_dict
+    ).T
+
+    route_delay_df.columns = [
+        "Avg_TTDays",
+        "delay"
+    ]
+
+    # get outliers
+    tt_col = route_delay_df["Avg_TTDays"].values
+
+    sigma = tt_col.std()
+    mu = tt_col.mean()
+    outlier_mask = np.abs(tt_col - mu) > 2*sigma
+
+    above_mean_mask = tt_col > mu
+
+
+    route_delay_df.loc[:, "outlier"] = outlier_mask
+
+    route_delay_df.loc[:, "above_mean"] = above_mean_mask
+
+
+    # generate outlier column
+
+
+
+    route_delay_df.reset_index(inplace=True)
+    route_delay_df.columns = [
+        "Month",
+        "Avg_TTDays",
+        "delay",
+        "outlier",
+        "above_mean"
+    ]
+
+    return route_delay_df
+
+
+Trade = 'Asia-North America East Coast'  #'Asia-MEA'
+col_label = "Avg_TTDays"
+rel_df_nona_delays = rel_df_nona.copy()
+
+delay_col = rel_df_nona_delays["OnTime_Reliability"]==0
+rel_df_nona_delays.loc[:, "delay"] = delay_col
+
+
+rel_df_nona_delays = rel_df_nona_delays[
+    rel_df_nona_delays["Trade"]==Trade
+]
+
+tt_delay_analysis_by_route = rel_df_nona_delays.groupby(
+    [
+        "POL",
+        "POD",
+        "Carrier",
+        "Service",
+        "Trade"
+    ]
+).apply(
+    lambda x: dict(zip(x["Month(int)"], zip(x[col_label], x["delay"])) )
+)
+
+
+route = tt_delay_analysis_by_route.index[200]
+
+delay_df = convert_to_frame(tt_delay_analysis_by_route[route])
+source = delay_df
+
+# alt.Chart(source).mark_circle(size=60).encode(
+#     x='Month',
+#     y='Avg_TTDays',
+#     color='delay',
+#     tooltip=['Avg_TTDays', 'delay', 'outlier']
+# ).interactive()
+
+
+
+
 
 # freight (carrier rates)
 
@@ -236,6 +389,32 @@ with st.sidebar:
 
 
 if plot:
+
+    no_samples = min(5000, res_df.shape[0])
+    bar_chart = alt.Chart(res_df.sample(no_samples), title=f"Outlier/Delay analysis for {trade_option}").mark_bar().encode(
+        # x=alt.X('Type', scale=alt.Scale(8), title=None),
+        x=alt.X('Type', title=None),
+        y=alt.Y('mean(Count)', title='Percentage'),
+        color='Type',
+        column='Month',
+        tooltip=['Type', 'mean(Count)']
+    ).configure_view(
+        stroke='transparent'
+    )
+    st.altair_chart(bar_chart)
+
+
+    # delay scatter
+    delay_scatter = alt.Chart(source, title=f"Sample Transit Time in \n{Trade}").mark_circle().encode(
+        x='Month',
+        y='Avg_TTDays',
+        color='delay',
+        tooltip=['Avg_TTDays', 'delay', 'outlier']
+    ).interactive()
+
+    st.altair_chart(delay_scatter, use_container_width=True)
+
+
     predictor = predictor_option
     freight_column = "Market Average"
     predictor_title = predictor_titles[predictor]
@@ -266,3 +445,6 @@ if plot:
     )
 
     st.altair_chart(c, use_container_width=True)
+
+
+
